3ce2721b1ee7d9c43205b557cee55fa91a9966bc
Patrick HENG <hengpatrick.pro@gmail.com>
2016-02-25 20:58:29 +0100

    WebGL - Add hand configuration

--- src/components/webgl-experience/Hand/index.js
+++ src/components/webgl-experience/Hand/index.js
@@ -1,5 +1,10 @@
 import ObjLoader from '../Utils/ObjLoader';
+
 import Container from 'Container';
+
+import HotSpot from '../HotSpot';
+
+import raf from 'raf';
 
 /**
  * Hand class
@@ -19,13 +24,32 @@ class Hand extends THREE.Object3D {
 
     this.loader = new THREE.OBJLoader(this.manager);
 
+    this.hotSpots = [];
+
     this.gui = Container.get('GUI');
 
-    this.position.set(100, -230, 0);
-    this.rotation.y = 0.9;
+    this.clock = Container.get('Clock');
+
+    this.configuration = Container.get('Configuration');
+
+    this.meshConfiguration = this.configuration.get('meshes.hand');
+
+    this.hotSpotsConfig = this.configuration.get('hotSpots.hand');
+
+    this.hotSpots = [];
+
+    this.position.set( this.meshConfiguration.position.x, this.meshConfiguration.position.y, this.meshConfiguration.position.z );
+
+    this.rotation.y = this.meshConfiguration.rotation.y;
+
+    this.bind();
 
     this.loadMesh();
 
+  }
+
+  bind() {
+    this.update = this.update.bind(this);
   }
 
   loadMesh() {
@@ -34,7 +58,7 @@ class Hand extends THREE.Object3D {
 
       this.mesh = object;
 
-      this.mesh.scale.set( 0.5, 0.5, 0.5 );
+      this.mesh.scale.set( this.meshConfiguration.scale.x, this.meshConfiguration.scale.y, this.meshConfiguration.scale.z );
 
       this.box = new THREE.Box3().setFromObject( this.mesh );
 
@@ -44,6 +68,10 @@ class Hand extends THREE.Object3D {
 
       this.initGUI();
 
+      this.generateHotSpots();
+
+      this.update();
+
     }, this.onLoadProgress, this.onLoadError);
   }
 
@@ -52,8 +80,27 @@ class Hand extends THREE.Object3D {
 
     folder.add(this.position, 'x');
     folder.add(this.position, 'y');
-    folder.add(this.position, 'z');
+    folder.add(this.position, 'z', -20, 2);
     folder.add(this.rotation, 'y').name('rotation y');
+
+  }
+
+  hotSpotDisplay() {
+    for (let i = 0; i < this.hotSpots.length; i++) {
+      setTimeout(()=> {
+        this.hotSpots[i].hotSpotDisplay();
+      }, 50)
+    }
+  }
+
+  generateHotSpots() {
+    for (let i = 0; i < this.hotSpotsConfig.length; i++) {
+      const hotSpot = new HotSpot(this.hotSpotsConfig[i]);
+
+      this.add(hotSpot);
+
+      this.hotSpots.push(hotSpot);
+    }
 
   }
 
@@ -62,6 +109,16 @@ class Hand extends THREE.Object3D {
   }
 
   onLoadError() {
+  }
+
+  update() {
+    const time = this.clock.time;
+
+    for (let i = 0; i < this.hotSpots.length; i++) {
+      this.hotSpots[i].update(time);
+    }
+
+    raf(this.update);
   }
 }
 
--- src/config/meshes.js
+++ src/config/meshes.js
@@ -5,6 +5,8 @@ export default {
     scale: new THREE.Vector3( 3.6, 3.6, 3.6 )
   },
   hand: {
-
+    position: new THREE.Vector3( 100, -230, 0 ),
+    rotation: new THREE.Vector3( 0, 0.9, 0 ),
+    scale: new THREE.Vector3( 0.5, 0.5, 0.5 )
   }
 }
